c1c833c85bdebe9e1057a92c8bc9c6597626ac5e
eashaan-rao <rao.eashaan@gmail.com>
2025-01-13 12:34:51 +0530

    Added feature_extraction.py and rvsm_model.py

---
+++ src/rvsm_model.py
@@ -0,0 +1,16 @@
+from util import csv2dict, tsv2dict, helper_collections, topk_accuracy
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split, KFold
+import numpy as np
+
+
+def rvsm_model():
+    samples = csv2dict("../data/features.csv")
+    rvsm_list = [float(sample["rVSM_similarity"]) for sample in samples]
+
+    # These collections are speed up the process while calculating top-k accuracy
+    sample_dict, bug_reports, br2files_dict = helper_collections(samples, True)
+
+    acc_dict = topk_accuracy(bug_reports, sample_dict, br2files_dict)
+
+    return acc_dict
