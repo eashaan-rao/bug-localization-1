56a1a07dca343a19a238a4272d154a311f149bd7
eashaan-rao <rao.eashaan@gmail.com>
2025-01-17 12:36:20 +0530

    Added methods to calculate six features for bug localization in util.py

--- src/util.py
+++ src/util.py
@@ -243,3 +243,130 @@ def bug_fixing_recency(br, prev_reports):
         )
     
     return 0
+
+def collaborative_filtering_score(raw_text, prev_reports):
+    '''
+    Calculates ...
+
+    Arguments:
+        raw_text {string} -- raw text of the bug report
+        prev_reports {list} -- list of previous reports
+    '''
+
+    prev_reports_merged_raw_text = ""
+    for report in prev_reports:
+        prev_reports_merged_raw_text += report["raw_text"]
+
+    cfs = cosine_sim(raw_text, prev_reports_merged_raw_text)
+
+    return cfs
+
+def class_name_similarity(raw_text, source_code):
+    '''
+    Calculates the class name present from bug reports and source code 
+    
+    Arguments:
+        raw_text {string} -- raw text of the bug report
+        source_code {string} -- java source code
+    '''
+
+    classes = source_code.split(" class ")[1:]
+    class_names = [c[: c.find(" ")] for c in classes]
+    class_names_text = " ".join(class_names)
+
+    class_name_sim = cosine_sim(raw_text, class_names_text)
+
+    return class_name_sim
+
+def helper_collections(samples, only_rvsm=False):
+    '''
+    Generates helper function for calculations
+
+    Arguments:
+        samples {list} -- samples from features.csv
+
+    keyword Arguments:
+        only_rvsm {bool} -- If True only 'rvsm' features are added to 'sample_dict'. (default: {False}) 
+    '''
+
+    sample_dict = {}
+    for s in samples:
+        sample_dict[s["report_id"]] = []
+
+    for s in samples:
+        temp_dict = {}
+        values = [float(s["rVSM_similarity"])]
+        if not only_rvsm:
+            values += [
+                float(s["collab_filter"]),
+                float(s["classname_similarity"]),
+                float(s["bug_recency"]),
+                float(s["bug_frequency"]),
+            ]
+        temp_dict[os.path.normpath(s["file"])] = values
+
+        sample_dict[s["report_id"]].append(temp_dict)
+    
+    bug_reports = tsv2dict("../data/Eclipse_Platform_UI.txt")
+    br2files_dict = {}
+
+    for bug_report in bug_reports:
+        br2files_dict[bug_report["id"]] = bug_report["files"]
+    
+    return sample_dict, bug_reports, br2files_dict
+
+def topk_accuracy(test_bug_reports, sample_dict, br2files_dict, clf=None):
+    '''
+    Calculates top-k accuracies
+
+    Arguments:
+        test_bug_reports {list of dictionaries} -- list of all bug reports
+        sample_dict {dictionary of dictionaries} -- a helper collection for fast accuracy calculation
+        br2files_dict {dictionary} -- dictionary for "bug report id - list of all related files in features.csv pairs
+
+    keyword arguments:
+        clf {object} -- A classifier with 'predict()' function. If none, rvsm relevancy is used (default: None)
+    
+    '''
+
+    topk_counters = [0] * 20
+    negative_total = 0
+    for bug_report in test_bug_reports:
+        dnn_input = []
+        corresponding_files = []
+        bug_id = bug_report["id"]
+
+        try:
+            for temp_dict in sample_dict[bug_id]:
+                java_file = list(temp_dict.keys())[0]
+                features_for_java_file = list(temp_dict.values())[0]
+                dnn_input.append(features_for_java_file)
+                corresponding_files.append(java_file)
+        except:
+            negative_total += 1
+            continue
+
+        # Calculate relevancy for all files related to the bug report in features.csv
+        # In features.csv, there are 50 wrong(randomly choosen) files for each right (buggy) file.
+        relevancy_list = []
+        if clf: # dnn classifier
+            relevancy_list = clf.predict(dnn_input)
+        else: # rVSM
+            relevancy_list = np.array(dnn_input).ravel()
+
+        # Top-1, top-2 .. top-20 accuracy
+        for i in range(1, 21):
+            max_indices = np.argpartition(relevancy_list, -i)[-i:]
+            for corresponding_file in np.array(corresponding_files)[max_indices]:
+                if str(corresponding_file) in br2files_dict[bug_id]:
+                    topk_counters[i - 1] += 1
+                    break
+    
+    acc_dict = {}
+    for i, counter in enumerate(topk_counters):
+        acc = counter / (len(test_bug_reports) - negative_total)
+        acc_dict[i + 1] += round(acc, 3)
+
+    return acc_dict
+
+        
