33cb6d25dd2745b1ce481bcac9a554cdc2ba4156
eashaan-rao <rao.eashaan@gmail.com>
2025-03-07 10:40:14 +0530

    Added functionalities of MAP and MRR

--- src/util.py
+++ src/util.py
@@ -408,7 +408,7 @@ def helper_collections(samples_df, only_rvsm=False):
 
 def topK_accuracy(test_bug_reports, sample_dict, br2files_dict, clf=None):
     '''
-    Calculates top-k accuracies
+    Calculates top-k accuracies for specified k valies (e.g. top-1, top-5, top-10)
 
     Arguments:
         test_bug_reports {list of dictionaries} -- list of all bug reports
@@ -418,9 +418,13 @@ def topK_accuracy(test_bug_reports, sample_dict, br2files_dict, clf=None):
     keyword arguments:
         clf {object} -- A classifier with 'predict()' function. If none, rvsm relevancy is used (default: None)
     
+    Returns:
+        dict -- Dictionary with top-k values as keys and accuracy as values
     '''
 
-    topk_counters = [0] * 20
+    top_k = [1, 5, 10]
+    # topk_counters = [0] * 20
+    topk_counters = {k: 0 for k in top_k}
     negative_total = 0
     for bug_report in test_bug_reports:
         dnn_input = []
@@ -433,7 +437,7 @@ def topK_accuracy(test_bug_reports, sample_dict, br2files_dict, clf=None):
                 features_for_java_file = list(temp_dict.values())[0]
                 dnn_input.append(features_for_java_file)
                 corresponding_files.append(java_file)
-        except:
+        except KeyError:
             negative_total += 1
             continue
 
@@ -446,12 +450,12 @@ def topK_accuracy(test_bug_reports, sample_dict, br2files_dict, clf=None):
             relevancy_list = np.array(dnn_input).ravel()
 
         # Top-1, top-2 .. top-20 accuracy
-        for i in range(1, 21):
+        for k in top_k:
             # max_indices = np.argpartition(relevancy_list, -i)[-i:]
-            max_indices = np.argsort(relevancy_list)[-i:]
+            max_indices = np.argsort(relevancy_list)[-k:]
             for corresponding_file in np.array(corresponding_files)[max_indices]:
                 if str(corresponding_file) in br2files_dict[bug_id]:
-                    topk_counters[i - 1] += 1
+                    topk_counters[k] += 1
                     break
     
     acc_dict = {}
@@ -460,11 +464,75 @@ def topK_accuracy(test_bug_reports, sample_dict, br2files_dict, clf=None):
         print("Warning: No valid bug reports found in sample_dict. Returning empty accuracy dictionary.")
         return acc_dict # Return an empty dictionary to prevent the error
     
-    for i, counter in enumerate(topk_counters):
-        acc = counter / denominator
-        acc_dict[i + 1] = round(acc, 3)
+    for k, counter in topk_counters.items():
+        acc_dict[k] = round(counter / denominator, 3)
 
     return acc_dict
+
+def calculate_MAP(test_bug_reports, sample_dict, br2files_dict, clf=None):
+    '''
+    Calculaates Mean Average Precision (MAP)
+    '''
+    average_precisions = []
+    for bug_report in test_bug_reports:
+        dnn_input = []
+        corresponding_files = []
+        bug_id = int(bug_report["id"])
+
+        try:
+            for temp_dict in sample_dict[bug_id]:
+                java_file = list(temp_dict.keys())[0]
+                features_for_java_file = list(temp_dict.values())[0]
+                dnn_input.append(features_for_java_file)
+                corresponding_files.append(java_file)
+        except KeyError:
+            continue
+
+        relevancy_list = clf.predict(dnn_input) if clf else np.array(dnn_input).ravel()
+        ranked_indices = np.argsort(relevancy_list)[::-1]
+
+        relevant_files = br2files_dict.get(bug_id, [])
+        hits, precision_at_i, rank = 0, 0.0, 0
+
+        for idx in ranked_indices:
+            rank += 1
+            if str(corresponding_files[idx]) in relevant_files:
+                hits += 1
+                precision_at_i += hits/rank
+            if hits > 0:
+                average_precisions.append(precision_at_i / hits)
+    return round(np.mean(average_precisions), 3) if average_precisions else 0.0 
+
+def calculate_MRR(test_bug_reports, sample_dict, br2files_dict, clf=None):
+    '''
+    Calculates Mean Reciprocal Rank (MRR)
+    '''
+    reciprocal_ranks = []
+    for bug_report in test_bug_reports:
+        dnn_input = []
+        corresponding_files = []
+        bug_id = int(bug_report["id"])
+
+        try:
+            for temp_dict in sample_dict[bug_id]:
+                java_file = list(temp_dict.keys())[0]
+                features_for_java_file = list(temp_dict.values())[0]
+                dnn_input.append(features_for_java_file)
+                corresponding_files.append(java_file)
+        except KeyError:
+            continue
+        
+        relevancy_list = clf.predict(dnn_input) if clf else np.array(dnn_input).ravel()
+        ranked_indices = np.argsort(relevancy_list)[::-1]
+
+        relevant_files = br2files_dict.get(bug_id, [])
+        
+        for rank, idx in enumerate(ranked_indices, start=1):
+            if str(corresponding_files[idx]) in relevant_files:
+                reciprocal_ranks.append(1.0 / rank)
+                break
+    return round(np.mean(reciprocal_ranks), 3) if reciprocal_ranks else 0.0
+
 
 class CodeTimer:
     '''
